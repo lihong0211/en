995aa10da3798cfedc132d5ac097c507b577aac3
黎洪 <lihong@lihongdeMacBook-Pro-2.local>
2024-01-12 12:45:43 +0800

    fix: 网安link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ import { css } from '@emotion/react';
 
 export default function AddDialogue() {
   const record = 'https://beian.miit.gov.cn/#/Integrated/index';
+  const safe = 'https://beian.mps.gov.cn/#/query/webSearch';
   return (
     <div
       className="absolute bottom-0 h-[50px] flex items-center justify-center w-full"
@@ -11,7 +12,7 @@ export default function AddDialogue() {
       <Button type="link" href={record}>
         备案号：蜀ICP备2024044574号
       </Button>
-      <Button type="link" href={record}>
+      <Button type="link" href={safe}>
         <img
           src="https://beian.mps.gov.cn//img/logo01.dd7ff50e.png"
           className="w-[15px] h-[16px] mr-1"
